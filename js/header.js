44255ea896a63a7b4b5d59b9344dce20ea7be950
Tabrez <myt0786@gmail.com>
2020-11-26 15:30:45 +0530

    minnor issues fixed

---
+++ js/header.js
@@ -0,0 +1,5 @@
+// var signIn_Up = document.getElementsById('sign');
+// signIn_Up.addEventListener('click', function(){
+//     document.getElementsById('exampleModalSignUp').style.display='none';
+//     document.getElementsById('exampleModal').style.display='block';
+// })
